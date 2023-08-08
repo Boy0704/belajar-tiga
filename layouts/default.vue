3ca422fd56d82654e37991aee2c2ffff2dfe4284
boy <boykurniawan123@gmail.com>
2023-08-08 20:25:42 +0700

    add template

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="wrapper">
+        <app-sidebar v-if="isAuth"></app-sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+			<div id="content">
+                <app-header v-if="isAuth"></app-header>
+                <nuxt/>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import SideBar from '../components/layouts/SideBar.vue';
+import Header from '../components/layouts/Header.vue';
+
+export default {
+    data() {
+        return {
+            isAuth: false
+        }
+    },
+    components: {
+        'app-sidebar' : SideBar,
+        'app-header' : Header
+    }    
+}
+</script>
